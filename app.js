730017ce69095490da9fef396266334b3a0c0a88
Carlos Santana <csantana@us.ibm.com>
2019-09-09 15:15:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody hope this works OMG!");
+  res.send("Hello from Appsody hope this works OMG Demo");
 });
 
 module.exports.app = app;
